e024b83721d4a0e79fe822101fa96a9ebf060683
“[Tom <tbragg88@hotmail.com>
2025-06-18 19:50:13 +0100

    add button working and chirping correctly

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,8 +1,21 @@
+// ----- HTML HOOK UP'S ------------
+
 document.getElementById("startButton").addEventListener("click", function() {
-    document.getElementById("menuScreen").style.display = "none";  
-    document.getElementById("gameScreen").style.display = "block"; 
+    document.getElementById("menuScreen").style.display = "none";  // Hide menu
+    document.getElementById("gameScreen").style.display = "block"; // Show game
 });
-// AI randomly selects a move
+
+//-------GAME RULES + FUNCTIONS --------
+
+// ok we start the game , we need the "ai to select his choice secretly"
+//player inputs his choice and its starts or requires button submit
+// game compares two answers and returns who wins
+// alert box or something to indicate winner?
+//scores to be alterated
+// "animation" of sad ai opponent and new game button ?
+
+//computer choice
+
 function aiAction() {
     const actions = ["rock", "paper", "scissors", "lizard", "spock"];
     return actions[Math.floor(Math.random() * actions.length)];
