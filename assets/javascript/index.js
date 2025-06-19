69568149db1bb86e8a94f5de2fd1931b772f6b37
“[Tom <tbragg88@hotmail.com>
2025-06-19 09:28:46 +0100

    fix: correct comment formatting in winner function section

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -40,8 +40,8 @@ let winner = function (playerAction, aiChoice) {
     }
     document.getElementById("message-el").textContent = message;
 };
-
-// --------buttons and winner function --------
+/
+// --------buttons and winner function -----------
 document
     .querySelector(".game-buttons")
     .addEventListener("click", function (event) {
